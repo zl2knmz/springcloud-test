b3561776e895ffaf1675e939cf25e42f8fe2f8fd
zl <837870929@qq.com>
2022-02-16 11:43:17 +0800

    init

---
+++ springcloud-alibaba/src/main/java/com/liang/TestApplication.java
@@ -0,0 +1,16 @@
+package com.liang;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * @author zl
+ */
+@SpringBootApplication
+public class TestApplication {
+
+	public static void main(String[] args) {
+        SpringApplication.run(TestApplication.class, args);
+	}
+
+}
