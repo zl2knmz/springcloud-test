fb64f2edce3dd7a9011c8f0337dde75d3a9a6aef
Zhongliang <837870929@qq.com>
2022-10-28 23:41:36 +0800

    1

---
+++ spring-aop/src/test/java/Test01.java
@@ -0,0 +1,13 @@
+import org.junit.Test;
+
+/**
+ * @author zl
+ * @date 2022/10/28 23:23
+ */
+public class Test01 {
+
+    @Test
+    public void test(){
+        System.out.println("11111111111");
+    }
+}
